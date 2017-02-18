43bfab4a3bd05e0bd302a1afb5df7c7219577865
yuppy-yamamo <14fi116@ms.dendai.ac.jp>
2017-02-19 04:51:06 +0900

    num.15

---
+++ Chapter2/Question15.py
@@ -0,0 +1,19 @@
+# Question15.py [filename] [n]
+import sys
+
+args = sys.argv
+length = len(args)
+
+filename = args[1]
+n = int(args[2])
+
+file = open(filename)
+lines = file.readlines()
+
+data = ""
+list_length = len(lines)
+
+for i in range(n):
+    line = int(list_length - i - 1)
+    data = lines[line] + data
+print(data)
